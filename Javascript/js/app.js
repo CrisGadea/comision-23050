10c4e30285fcf4e96bbbdbadf0ce814b293c4265
CrisGadea <cristianhernangadea@gmail.com>
2023-04-19 20:06:54 -0300

    Clase Intro JS

---
+++ Javascript/js/app.js
@@ -0,0 +1,27 @@
+//alert("Hello World");
+
+// Variables declaradas con var
+var dni;
+
+var nombre = "Jose";
+
+var apellido = "Doe"; 
+
+var edad = 18;
+
+// Variables declaradas con let
+
+let email = "mail@mail.com";
+
+nombre = "Hernán";
+
+const ANIO_NACIMIENTO = 1996;
+
+var numero1 = "12";
+
+var numero2 = 13;
+
+
+// El operador +, en textos, se usa para concatenar (pegar textos)
+alert("Hola, soy " + nombre + " " + apellido + ", tengo " + edad + " años y nací en el año: " + ANIO_NACIMIENTO);
+alert("La suma da como resultado = " + (parseInt(numero1) + numero2));
